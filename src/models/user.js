f1f50ec7cb5981fd883f65845f4c9a02166c7980
lamquangho <holam24062003@gmail.com>
2025-08-25 21:12:50 +0700

    fix login

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
@@ -8,5 +9,22 @@ const userSchema = new mongoose.Schema({
   phone: { type: String },
   address: { type: String },
 }, { timestamps: true });
+
+// Mã hóa mật khẩu trước khi lưu
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Thêm method comparePassword
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
