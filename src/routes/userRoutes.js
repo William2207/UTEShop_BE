2a354a54bb47b0359b52f4ff5677c8198cb411bf
William2207 <122082110+William2207@users.noreply.github.com>
2025-08-26 16:43:08 +0700

    fix profile api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { requireAuth } from "../middlewares/auth.js";
 import User from "../models/user.js";
-import UserController from "../controllers/userController.js";
+import UserController from "../controllers/UserController.js";
 
 const router = express.Router();
 
