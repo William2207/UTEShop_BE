f021cac4aa130727f380ad9e4da811fbbba7d1c0
William2207 <122082110+William2207@users.noreply.github.com>
2025-08-31 16:00:23 +0700

    Change Profile info, password, avatar api

--- src/models/user.js
+++ src/models/user.js
@@ -1,35 +1,44 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  name: {                         // dùng name làm cột chính
-    type: String,
-    required: [true, 'Name is required'],
-    minlength: 2,
-    trim: true,
-    alias: 'username',            // alias để code cũ dùng username vẫn chạy
+const userSchema = new mongoose.Schema(
+  {
+    avatarUrl: { type: String, default: "" },
+    name: {
+      // dùng name làm cột chính
+      type: String,
+      required: [true, "Name is required"],
+      minlength: 2,
+      trim: true,
+      alias: "username", // alias để code cũ dùng username vẫn chạy
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true, minlength: 6 },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    phone: { type: String },
+    address: { type: String },
+    birthDate: {
+      type: Date,
+      required: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: { type: String, required: true, minlength: 6 },
-  role: { type: String, enum: ["customer", "admin"], default: "customer" },
-  phone: { type: String },
-  address: { type: String },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-userSchema.pre('save', async function(next){
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.comparePassword = function(plain){
+userSchema.methods.comparePassword = function (plain) {
   return bcrypt.compare(plain, this.password);
 };
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
